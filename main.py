1bb12335a0fb0e1c2c5cbf0fbb84ed0f5b0f864f
Sweeton Fernandes <37552869+SNFernandes24@users.noreply.github.com>
2022-02-01 18:07:28 +0000

    Add email support

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 #! python3
+import imp
 from googleapiclient.errors import HttpError
 from redditFetch import getTwitchClips
 from uploadToYoutube import get_authenticated_service, initialize_upload
 from automateDownload import autoDownloadFile
 from clearFiles import clearDirectory
 from Secret.videoOptions import CATEGORY, DESCRIPTION, KEYWORDS, PRIVACYSTATUS
+from sendEmail import sendEmail
 
 def runMain():
-    args = {'description' : DESCRIPTION,
-    'category' : CATEGORY,
-    'keywords' : KEYWORDS,
-    'privacyStatus' : PRIVACYSTATUS}
+    sendEmail()
+    # args = {'description' : DESCRIPTION,
+    # 'category' : CATEGORY,
+    # 'keywords' : KEYWORDS,
+    # 'privacyStatus' : PRIVACYSTATUS}
 
-    youtube = get_authenticated_service()
-    clip_list = getTwitchClips()
+    # youtube = get_authenticated_service()
+    # clip_list = getTwitchClips()
     
-    for clip in clip_list:
+    # for clip in clip_list:
         
-        fileName = autoDownloadFile(clip_list[clip])
-        print(type(fileName))
-        if fileName != None:
-            try:
-                initialize_upload(youtube, args, clip, fileName)
-            except HttpError as e:
-                print ('An HTTP error %d occurred:\n%s') % (e.resp.status, e.content) 
-    clearDirectory()
+    #     fileName = autoDownloadFile(clip_list[clip])
+    #     print(type(fileName))
+    #     if fileName != None:
+    #         try:
+    #             initialize_upload(youtube, args, clip, fileName)
+    #         except HttpError as e:
+    #             print ('An HTTP error %d occurred:\n%s') % (e.resp.status, e.content) 
+    # clearDirectory()
 
 if __name__ == "__main__":    
     runMain()
